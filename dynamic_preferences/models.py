df24bb8b3c39cda961f209ef78d8cf757cded2e7
Christopher Valerio <christopher@valerio.guru>
2018-03-21 16:32:43 -0600

    making properties callable

--- dynamic_preferences/models.py
+++ dynamic_preferences/models.py
@@ -36,15 +36,13 @@ class BasePreferenceModel(models.Model):
         return self.registry.get(
             section=self.section, name=self.name, fallback=True)
 
-    def _verbose_name(self):
+    @property
+    def verbose_name(self):
         return self.preference.get('verbose_name', self.preference.identifier)
-    _verbose_name.short_description = "Nombre"
-    verbose_name = property(_verbose_name)
 
-    def _help_text(self):
+    @property
+    def help_text(self):
         return self.preference.get('help_text', '',)
-    _help_text.short_description = "Texto ayuda"
-    help_text = property(_help_text)
 
     def set_value(self, value):
         """
